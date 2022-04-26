e87372ecded8d671aae9176b1cac49b7b14bfe66
Alexandre <alexandrepereon8@gmail.com>
2022-04-26 17:39:38 +0200

    NewWebSite

---
+++ JS/test.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#SignInSubmit').click(function() {
+        $('#spacer').animate({
+            padding: '3vh'
+        },2000)
+
+        $('#wrapper').animate({
+            width: '70%'
+        },2000)
+    })
+});
